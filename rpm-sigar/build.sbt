9ad9932dd95d5622b844618b0b963f6a21dcd3fa
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2014-11-06 17:12:20 +0100

    Adding example with sigar and rpm packaging #1

---
+++ rpm-sigar/build.sbt
@@ -0,0 +1,26 @@
+import com.typesafe.sbt.SbtNativePackager._
+import NativePackagerKeys._
+
+name := "simple-rpm"
+
+version := "1.0"
+
+mainClass in Compile := Some("de.mukis.MainApp")
+
+packageArchetype.java_server
+
+maintainer in Linux := "Nepomuk Seiler <nepomuk.seiler@mukis.de>"
+
+packageSummary in Linux := "Custom startscript parameters"
+
+packageDescription := "Custom startscript parameters"
+
+rpmRelease := "1"
+
+rpmVendor := "mukis.de"
+
+rpmUrl := Some("http://github.com/muuki88/sbt-native-packager")
+
+rpmLicense := Some("Apache v2")
+
+packageArchitecture in Rpm := "x86_64"
---
+++ rpm-sigar/src/main/scala/de/mukis/MainApp.scala
@@ -0,0 +1,34 @@
+package de.mukis
+
+import java.util.concurrent.{ Executors, TimeUnit }
+import java.nio.file.Files
+import java.nio.file.StandardOpenOption._
+import java.nio.charset.Charset
+
+object MainApp extends App {
+
+  val logFile = Files createTempFile ("mukis-", ".log")
+  val log = Files newBufferedWriter (logFile, Charset forName "UTF-8")
+  log write (s"Parameters:\t ${args mkString " "}")
+  log newLine ()
+  log write (s"System property: ${System.getProperty("de.mukis", "Property 'de.mukis' not found")}")
+  log newLine ()
+  log.close
+
+  val interval = args.indexOf("-interval") match {
+    case i if args.size >= i => args(i + 1).toInt
+    case _                   => 10
+  }
+
+  // Creating an executor service to schedule the config parsing
+  val service = Executors newScheduledThreadPool 1
+  service.scheduleAtFixedRate(new Runnable() {
+    override def run() {
+      val log = Files newBufferedWriter (logFile, Charset forName "UTF-8", APPEND)
+      log write (s"Update:\t${System currentTimeMillis}")
+      log.newLine
+      log.close
+    }
+  }, 0, interval, TimeUnit.SECONDS)
+
+}
